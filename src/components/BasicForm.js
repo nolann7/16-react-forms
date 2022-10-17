26a5c90012fae8d6e5882dd13072171d14f3457e
nolann7 <nolann709@gmail.com>
2022-10-17 15:32:59 +0300

    делаю логику валидации для basicform для firstname

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,69 @@
-const BasicForm = (props) => {
+import { useState } from 'react';
+
+const BasicForm = () => {
+  const [enteredFirstName, setEnteredFirstName] = useState('');
+  const [firstNameInputIsTouched, setFirstNameInputIsTouched] = useState(false);
+
+  const enteredFirstNameIsValid = enteredFirstName.trim() !== '';
+  const fistNameInputHasError = !enteredFirstNameIsValid && firstNameInputIsTouched;
+
+  let formIsValid = false;
+  if (enteredFirstNameIsValid) {
+    formIsValid = true;
+  }
+
+  const firstNameInputChangeHandler = e => {
+    setEnteredFirstName(e.target.value);
+  };
+  const firstNameInputBlurHandler = () => {
+    setFirstNameInputIsTouched(true);
+  }
+
+  const formSubmitHandler = e => {
+    e.preventDefault();
+
+    if (!formIsValid) return;
+
+    console.log(enteredFirstName);
+
+    setEnteredFirstName('')
+    setFirstNameInputIsTouched(false)
+
+  }
+
+  const firstNameClassIsValid = fistNameInputHasError
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="control-group">
+        <div className={firstNameClassIsValid}>
+          <label htmlFor="first-name">First Name</label>
+          <input
+            type="text"
+            id="first-name"
+            onChange={firstNameInputChangeHandler}
+            onBlur={firstNameInputBlurHandler}
+            value={enteredFirstName}
+          />
+          {fistNameInputHasError && <p className='error-text'>Please enter valid First Name</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        {/* <div className={lastNameClassIsValid}> */}
+        <div>
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" />
+          {/* {lastNameInputHasError && <p className='error-text'>Please enter valid Last Name</p>} */}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      {/* <div className={emailClassIsValid}> */}
+      <div>
+        <label htmlFor="email">E-Mail Address</label>
+        <input type="email" id="email" />
+        {/* {emailInputHasError && <p className='error-text'>Please enter valid Email</p>} */}
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
